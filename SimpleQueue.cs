6ae7d2a6b157fef6d5a5c8b837ac10ebe9cb3f73
viav <viav@bk.ru>
2019-07-01 00:07:57 +0300

    - Simple queue

---
+++ SimpleQueue.cs
@@ -0,0 +1,72 @@
+﻿namespace DSaA
+{
+    class SimpleQueue<T>
+    {
+        T[] queue;
+        private int frontIndex;
+        private int backIndex;
+
+        public SimpleQueue(int size)
+        {
+            queue = new T[size];
+            frontIndex = -1;
+            backIndex = -1;
+        }
+
+        public bool enqueue(T val)
+        {
+            if(isFull())
+            {
+                return false;
+            }
+            else
+            {
+                if(frontIndex < 0)
+                {
+                    frontIndex = 0;
+                }
+
+                backIndex++;
+                queue[backIndex] = val;
+                return true;
+            }
+        }
+
+        public T dequeue()
+        {
+            if(isEmpty())
+            {
+                return default(T);
+            }
+            else
+            {
+                T data = queue[frontIndex];
+                queue[frontIndex] = default(T);
+                frontIndex++;
+                return data;
+            }
+        }
+
+        public T peek()
+        {
+            if(!isEmpty())
+            {
+                return queue[frontIndex];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public bool isFull()
+        {
+            return backIndex == queue.Length - 1;
+        }
+
+        public bool isEmpty()
+        {
+            return frontIndex < 0 || frontIndex > backIndex;
+        }
+    }
+}
