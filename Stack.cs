8107ecc90ca8d8241aa0ea41c87c357dd88751e5
viav <viav@bk.ru>
2019-07-01 00:13:58 +0300

    - Stack

---
+++ Stack.cs
@@ -0,0 +1,62 @@
+﻿namespace DSaA
+{
+    class Stack
+    {
+        private int[] stack;
+        private int topIndex;
+        private int size;
+
+        public Stack(int size)
+        {
+            stack = new int[size];
+            topIndex = -1;
+            this.size = size;
+        }
+
+        public void push(int val)
+        {
+            if(!isFull())
+            {
+                topIndex++;
+                stack[topIndex] = val;
+            }
+        }
+
+        public int pop()
+        {
+            if(!isEmpty())
+            {
+                int val = stack[topIndex];
+                stack[topIndex] = 0;
+                topIndex--;
+                return val;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int peek()
+        {
+            if(!isEmpty())
+            {
+                return stack[topIndex];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool isFull()
+        {
+            return topIndex == size - 1;
+        }
+
+        public bool isEmpty()
+        {
+            return topIndex < 0;
+        }
+    }
+}
